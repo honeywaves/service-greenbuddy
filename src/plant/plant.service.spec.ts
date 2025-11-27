86a19f8453ee02faa87a2431e15f3c3521c415e8
Chriiiiiss <chris.fraanc@gmail.com>
2025-11-27 21:53:15 +0100

    test(plant): Add unit tests for getPlantById method in PlantService, including error handling for non-existent plants

--- src/plant/plant.service.spec.ts
+++ src/plant/plant.service.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PlantService } from './plant.service';
 import { plants } from 'src/plants.data';
+import { NotFoundException } from '@nestjs/common/exceptions/not-found.exception';
 
 describe('PlantService', () => {
   let service: PlantService;
@@ -51,6 +52,37 @@ describe('PlantService', () => {
 
       expect(res2.id).toBe(2);
       expect(plants[1]).toBe(res2);
+    });
+  });
+
+  describe('getPlantById', () => {
+    beforeEach(() => {
+      plants.length = 0;
+      service.createPlant();
+      service.createPlant();
+      service.createPlant();
+    });
+
+    it('should get a plant by id', () => {
+      const plant = service.getPlantById(1);
+      expect(plant).toBeDefined();
+      expect(plant.id).toBe(1);
+      expect(plant.name).toBe('Plant 1');
+      expect(plant.mood).toBe('happy');
+      expect(plant.lastActionAt).toBeInstanceOf(Date);
+    });
+
+    it('should get a plant by id and return the correct plant', () => {
+      const plant = service.getPlantById(3);
+      expect(plant).toBeDefined();
+      expect(plant.id).toBe(plants[2].id);
+      expect(plant.name).toBe(plants[2].name);
+      expect(plant.mood).toBe(plants[2].mood);
+      expect(plant.lastActionAt).toBe(plants[2].lastActionAt);
+    });
+
+    it('should throw an error if the plant is not found', () => {
+      expect(() => service.getPlantById(4)).toThrow(NotFoundException);
     });
   });
 });
