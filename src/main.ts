4e68cc7f534d05c715341b690ff7b63acff2b4bc
Chriiiiiss <chris.fraanc@gmail.com>
2025-12-01 23:01:14 +0100

    chore(env): Update .env.example and main.ts to include PORT and HOST configuration for improved service startup

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,19 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const port = process.env.PORT ?? 3000;
+  const host = process.env.HOST ?? '0.0.0.0';
+
+  console.log(
+    `🌱 GreenBuddy Service will be starting on http://${host}:${port} from ${process.env.NODE_ENV} environment
+    host from env is ${process.env.HOST}, port from env is ${process.env.PORT}`,
+  );
+
+  app.enableCors({
+    origin: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    credentials: true,
+  });
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -15,6 +28,9 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(port, host);
+  console.log(`DEBUG: host=${host}, port=${port}`);
+  const url = await app.getUrl();
+  console.log(`🌱 GreenBuddy Service is running on ${url}`);
 }
 bootstrap();
